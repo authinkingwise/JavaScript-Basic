33e78ba0b14002425da5dbcbf729c80b020de071
Terry Ye <yetewise@gmail.com>
2018-06-25 23:54:59 +1000

    Add some more comment

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# JavaScript-Basic
+#This is a repo to record down basic of JavaScript
+
+#e.g. difference of DOM,BOM
+#e.g. difference of let, const, var to define variables
+#e.g. how to manipulate DOM element in JavaScript
--- letvar.js
+++ letvar.js
@@ -1,5 +1,6 @@
 
 'use strict';
+//"use strict"; Defines that JavaScript code should be executed in "strict mode".
 function TestVarLetDifference() { 
 
     let he = 201;
@@ -7,18 +8,20 @@ function TestVarLetDifference() {
 
         var a = 2;
     }
-
+    //define a variable with var will have scope leak issue, it can be accessed outside
+    //for loop as it is a functional scope variable
     console.log(i);
     console.log(a);
     console.log(he);
-
+    
     for (let j=0;j<10; j++){
 
         let b = 2;
     }
-
+    //J cannot be accessed as it is not defined, a ReferenceError will be prompted
+    //Same for b as it is not defined outside the loop
     console.log(j);
-    console.log(b);   
+    console.log(b); 
 }
 
 TestVarLetDifference()
